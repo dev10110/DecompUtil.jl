d14eacf4018507908cb05a70942a94b58ddfb472
Devansh Ramgopal Agrawal <devansh@umich.edu>
2023-02-23 00:08:35 -0500

    wrote wrapper

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
 using DecompUtil
 using Test
 
-@testset "DecompUtil.jl" begin
-    # Write your tests here.
+@testset "DecompUtil.jl - 2D Seed Decomposition" begin
+    
+
+    pos = [0,0.]
+    obs = [[-0.2, 1.5], [1, 0.], [0.8,-1.], [ -0.5, -0.5]]
+    bbox = [2,2.]
+
+    res = DecompUtil.seedDecomp_2D(pos, obs, bbox, 0.1)
+
+    println(res)
+
+    @test length(res.vs) == 8
+
+    cons = DecompUtil.constraints(res)
+
+    println(cons.A)
+
+    println(cons.b)
+
 end
